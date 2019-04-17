75d56d8318c7e22933b7a5059a2c3d5486f170bc
feifeicanfly <785301424@qq.com>
2019-04-17 22:03:05 +0800

    ribbon

--- microservice-simple-consumer-ribbon/src/main/java/com/itmuch/com/RibbonApplication.java
+++ microservice-simple-consumer-ribbon/src/main/java/com/itmuch/com/RibbonApplication.java
@@ -2,17 +2,19 @@ package com.itmuch.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class ConsumerMovieApplication {
+public class RibbonApplication {
   @Bean
+  @LoadBalanced
   public RestTemplate restTemplate() {
     return new RestTemplate();
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(ConsumerMovieApplication.class, args);
+    SpringApplication.run(RibbonApplication.class, args);
   }
 }
