1220ee3f37708f30a0e4cd3bbcae3574ecd2bc5d
feifeicanfly <raner1218@163.com>
2019-04-22 23:47:16 +0800

    feign-config-properties

---
+++ microservice-simple-consumer-feign-config-properties/src/main/java/com/itmuch/com/FeignConfigPropertiesApplication.java
@@ -0,0 +1,20 @@
+package com.itmuch.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableFeignClients
+@SpringBootApplication
+public class FeignConfigPropertiesApplication {
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeignConfigPropertiesApplication.class, args);
+  }
+}
