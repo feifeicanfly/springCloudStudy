acb714cb7dfca4cf331f005fde8c86de01438940
feifeicanfly <785301424@qq.com>
2019-09-30 09:50:44 +0800

    feign Hystrix监控

--- microservice-simple-consumer-movie-feign-hystrix-common/src/main/java/com/itmuch/com/ConsumerMovieApplication.java
+++ microservice-simple-consumer-movie-feign-hystrix-common/src/main/java/com/itmuch/com/ConsumerMovieApplication.java
@@ -2,11 +2,17 @@ package com.itmuch.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableTurbine
 @SpringBootApplication
 public class ConsumerMovieApplication {
   @Bean
