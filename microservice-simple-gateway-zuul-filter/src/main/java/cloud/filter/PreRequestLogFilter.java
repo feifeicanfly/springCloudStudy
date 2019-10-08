2b8496a71ea4c68e03cd7e9e9cca7a843a7bc36d
feifeicanfly <785301424@qq.com>
2019-10-08 15:54:11 +0800

    zuul自定义过滤器

---
+++ microservice-simple-gateway-zuul-filter/src/main/java/cloud/filter/PreRequestLogFilter.java
@@ -0,0 +1,39 @@
+package cloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/** 自定义过滤器 */
+@Slf4j
+@Component
+public class PreRequestLogFilter extends ZuulFilter {
+
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  @Override
+  public int filterOrder() {
+    return 1;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  @Override
+  public Object run() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    HttpServletRequest request = ctx.getRequest();
+    log.info(
+        String.format(
+            "send %s request to %s", request.getMethod(), request.getRequestURL().toString()));
+    return null;
+  }
+}
